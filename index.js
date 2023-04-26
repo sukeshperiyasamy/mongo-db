b953dc9905af83a11628e09c4bdfe04c40011018
sukesh <85454574+sukeshperiyasamy@users.noreply.github.com>
2023-04-26 13:40:17 +0530

    hello world code

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:admin@sukeshdb.qkgkjzh.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
